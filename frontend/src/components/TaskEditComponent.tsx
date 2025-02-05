b0efa05ad4c1ecef6eee753158115c5277dbd508
Sebastian <sebastian.varelagi@gmail.com>
2025-02-05 07:38:17 -0500

    feat: add task edit component

---
+++ frontend/src/components/TaskEditComponent.tsx
@@ -0,0 +1,167 @@
+import { ChevronLeft } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import { taskApi } from "../api/taskApi";
+import { CreateTaskDTO } from "../types/Task";
+import { useEffect, useState } from "react";
+
+interface FormState {
+  isLoading: boolean;
+  error: string | null;
+}
+
+type TaskParams = {
+  taskId: string;
+}
+
+export const TaskEditComponent = () => {
+  const navigate = useNavigate();
+  
+  const { taskId } = useParams() as TaskParams;
+  
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors },
+    reset 
+  } = useForm<CreateTaskDTO>({
+    defaultValues: {
+      title: '',
+      description: '',
+      completed: false
+    },
+  });
+
+  const [formState, setFormState] = useState<FormState>({
+    isLoading: false,
+    error: null
+  });
+
+
+  useEffect(() => {
+    const loadTask = async () => {
+      if (!taskId) {
+        navigate('/');
+        return;
+      }
+
+      try {
+        const loadedTask = await taskApi.getTask(+taskId);
+
+        reset({
+          title: loadedTask.title,
+          description: loadedTask.description,
+          completed: loadedTask.completed
+        });
+      } catch (e) {
+        toast.error(e instanceof Error ? e.message : 'Ocurrio un error al cargar la tarea');
+        navigate('/');
+      }
+    };
+
+    loadTask();
+  }, [taskId, navigate, reset]);
+
+  const onSubmit = async (data: CreateTaskDTO) => {
+    setFormState({ isLoading: true, error: null });
+
+    try {
+      console.log(data);
+      await taskApi.updateTask(taskId, data);
+      toast.success('Tarea actualizada correctamente.');
+      reset();
+      navigate('/');
+    } catch (error) {
+      setFormState({
+        isLoading: false,
+        error: error instanceof Error ? error.message : 'Hubo un error al crear la tarea'
+      });
+      toast.error('Error al crear la tarea');
+    }
+  };
+
+  return (
+    <>
+      <div className="mb-4">
+        <Link to={'/'} className="text-white text-decoration-none">
+          <ChevronLeft width={20} height={20} />
+          Regresar
+        </Link>
+      </div>
+      
+      <h3>Editar tarea</h3>
+
+      <form onSubmit={handleSubmit(onSubmit)} noValidate style={{ maxWidth: '400px' }}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">Título</label>
+          <input
+            type="text"
+            className={`form-control ${errors.title ? 'is-invalid' : ''}`}
+            id="title"
+            placeholder="Escribe el título de la tarea"
+            {...register('title', {
+              required: 'El título es requerido',
+              minLength: {
+                value: 3,
+                message: 'El título debe tener al menos 3 caracteres'
+              },
+              maxLength: {
+                value: 50,
+                message: 'El título no puede exceder los 20 caracteres'
+              }
+            })}
+          />
+          {errors.title && (
+            <div className="invalid-feedback">
+              {errors.title.message}
+            </div>
+          )}
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">Descripción</label>
+          <textarea
+            className={`form-control ${errors.description ? 'is-invalid' : ''}`}
+            style={{ maxHeight: '500px' }}
+            id="description"
+            placeholder="Escribe la descripción de la tarea (máx 100 carácteres)"
+            maxLength={100}
+            {...register('description', {
+              maxLength: {
+                value: 100,
+                message: 'La descripción no puede exceder los 100 caracteres'
+              }
+            })}
+            rows={3}
+          />
+          {errors.description && (
+            <div className="invalid-feedback">
+              {errors.description.message}
+            </div>
+          )}
+        </div>
+
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="completed"
+            {...register('completed')}
+          />
+          <label className="form-check-label" htmlFor="completed">
+            Completada
+          </label>
+        </div>
+
+        <button 
+          type="submit" 
+          className="btn btn-primary"
+          disabled={formState.isLoading}
+        >
+          {formState.isLoading ? 'Cargando...' : 'Actualizar Tarea'}
+        </button>
+      </form>
+    </>
+  );
+}
